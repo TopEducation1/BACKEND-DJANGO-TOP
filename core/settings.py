6b4b5a2de56edb3a87eb0ebad10b08cb994ab6ac
TopEducation1 <topeducationoficial@gmail.com>
2024-11-12 11:49:03 -0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
 
 # Configuración de CORS
 CORS_ALLOWED_ORIGINS = [
-    "http://frontend-react-top.railway.internal",
+    "https://frontend-react-top-production.up.railway.app/",
     "http://localhost:8080",
     "http://localhost:3000",
     "http://127.0.0.1:3000" 
